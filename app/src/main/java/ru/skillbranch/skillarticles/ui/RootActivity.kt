1d88cf0ed73aba8f59eee001dc95e84fbb97baad
quanik <le-cri-du-coer@yandex.ru>
2020-08-02 23:07:36 +0300

    First and second task

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
@@ -0,0 +1,139 @@
+package ru.skillbranch.skillarticles.ui
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_LONG
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_root.boottombar
+import kotlinx.android.synthetic.main.activity_root.coordinator_container
+import kotlinx.android.synthetic.main.activity_root.submenu
+import kotlinx.android.synthetic.main.activity_root.toolbar
+import kotlinx.android.synthetic.main.activity_root.tv_text_content
+import kotlinx.android.synthetic.main.layout_bottombar.btn_bookmark
+import kotlinx.android.synthetic.main.layout_bottombar.btn_like
+import kotlinx.android.synthetic.main.layout_bottombar.btn_settings
+import kotlinx.android.synthetic.main.layout_bottombar.btn_share
+import kotlinx.android.synthetic.main.layout_submenu.btn_text_down
+import kotlinx.android.synthetic.main.layout_submenu.btn_text_up
+import kotlinx.android.synthetic.main.layout_submenu.switch_mode
+import ru.skillbranch.skillarticles.R
+import ru.skillbranch.skillarticles.R.layout
+import ru.skillbranch.skillarticles.extensions.dpToIntPx
+import ru.skillbranch.skillarticles.viewmodels.ArticleState
+import ru.skillbranch.skillarticles.viewmodels.ArticleViewModel
+import ru.skillbranch.skillarticles.viewmodels.base.Notify
+import ru.skillbranch.skillarticles.viewmodels.base.Notify.ActionMessage
+import ru.skillbranch.skillarticles.viewmodels.base.Notify.ErrorMessage
+import ru.skillbranch.skillarticles.viewmodels.base.Notify.TextMessage
+import ru.skillbranch.skillarticles.viewmodels.base.ViewModelFactory
+
+class RootActivity : AppCompatActivity() {
+    private lateinit var viewModel: ArticleViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_root)
+        setupToolbar()
+        setupBottomBar()
+        setupSubmenu()
+
+        val vmFactory = ViewModelFactory("0")
+        viewModel = ViewModelProviders.of(this, vmFactory).get(ArticleViewModel::class.java)
+        viewModel.observeState(this) {
+            renderUi(it)
+        }
+        viewModel.observeNotifications(this) {
+            renderNotification(it)
+        }
+    }
+
+    private fun renderNotification(notify: Notify) {
+        val snackbar = Snackbar.make(coordinator_container, notify.message, LENGTH_LONG)
+            .setAnchorView(boottombar)
+            .setActionTextColor(getColor(R.color.color_accent_dark))
+
+        when (notify) {
+            is TextMessage -> {
+            }
+            is ActionMessage -> {
+                with(snackbar) {
+                    setAction(notify.actionLabel) {
+                        notify.actionHandler.invoke()
+                    }
+                        .setActionTextColor(getColor(R.color.color_accent_dark))
+
+                }
+            }
+            is ErrorMessage -> {
+                with(snackbar) {
+                    setBackgroundTint(getColor(R.color.design_default_color_error))
+                    setTextColor(getColor(android.R.color.white))
+                    setActionTextColor(getColor(android.R.color.white))
+                    setAction(notify.errLabel) {
+                        notify.errHandler?.invoke()
+                    }
+                }
+            }
+        }
+
+        snackbar.show()
+    }
+
+    private fun setupSubmenu() {
+        btn_text_up.setOnClickListener { viewModel.handleUpText() }
+        btn_text_down.setOnClickListener { viewModel.handleDownText() }
+        switch_mode.setOnClickListener { viewModel.handleNightMode() }
+    }
+
+    private fun setupBottomBar() {
+        btn_like.setOnClickListener { viewModel.handleLike() }
+        btn_bookmark.setOnClickListener { viewModel.handleBookmark() }
+        btn_share.setOnClickListener { viewModel.handleShare() }
+        btn_settings.setOnClickListener { viewModel.handleToggleMenu() }
+    }
+
+    private fun renderUi(data: ArticleState) {
+        btn_settings.isChecked = data.isShowMenu
+        if (data.isShowMenu) submenu.open() else submenu.close()
+
+        btn_like.isChecked = data.isLike
+        btn_bookmark.isChecked = data.isBookmark
+
+        switch_mode.isChecked = data.isDarkMode
+        delegate.localNightMode = if (data.isDarkMode) AppCompatDelegate.MODE_NIGHT_YES
+        else AppCompatDelegate.MODE_NIGHT_NO
+
+        if (data.isBigText) {
+            tv_text_content.textSize = 18f
+            btn_text_up.isChecked = true
+            btn_text_down.isChecked = false
+        } else {
+            tv_text_content.textSize = 14f
+            btn_text_up.isChecked = false
+            btn_text_down.isChecked = true
+        }
+
+        tv_text_content.text = if (data.isLoadingContent) "loading" else data.content.first() as String
+
+        toolbar.title = data.title ?: "loading"
+        toolbar.subtitle = data.category ?: "loading"
+        if (data.categoryIcon != null) toolbar.logo = getDrawable(data.categoryIcon as Int)
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val logo = if (toolbar.childCount > 2) toolbar.getChildAt(2) as ImageView else null
+        logo?.scaleType = ImageView.ScaleType.CENTER_CROP
+        val lp = logo?.layoutParams as androidx.appcompat.widget.Toolbar.LayoutParams
+        lp.let {
+            it.width = this.dpToIntPx(40)
+            it.height = this.dpToIntPx(40)
+            it.marginEnd = this.dpToIntPx(16)
+            logo.layoutParams = it
+        }
+    }
+}
---
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
@@ -0,0 +1,160 @@
+package ru.skillbranch.skillarticles.viewmodels
+
+import androidx.lifecycle.LiveData
+import ru.skillbranch.skillarticles.data.ArticleData
+import ru.skillbranch.skillarticles.data.ArticlePersonalInfo
+import ru.skillbranch.skillarticles.data.repositories.ArticleRepository
+import ru.skillbranch.skillarticles.extensions.data.toAppSettings
+import ru.skillbranch.skillarticles.extensions.data.toArticlePersonalInfo
+import ru.skillbranch.skillarticles.extensions.format
+import ru.skillbranch.skillarticles.viewmodels.base.BaseViewModel
+import ru.skillbranch.skillarticles.viewmodels.base.Notify
+import ru.skillbranch.skillarticles.viewmodels.base.Notify.TextMessage
+
+class ArticleViewModel(val articleId: String)
+    : BaseViewModel<ArticleState>(ArticleState()), IArticleViewModel {
+    private val repository = ArticleRepository
+
+    init {
+        subscribeOnDataSource(getArticleData()) { article,  state ->
+            article ?: return@subscribeOnDataSource null
+            state.copy(
+                author = article.author,
+                shareLink = article.shareLink,
+                title = article.title,
+                category = article.category,
+                categoryIcon = article.categoryIcon,
+                date = article.date.format()
+            )
+        }
+
+        subscribeOnDataSource(getArticleContent()) { content, state ->
+            content ?: return@subscribeOnDataSource null
+            state.copy(
+                isLoadingContent =  false,
+                content = content
+            )
+        }
+        subscribeOnDataSource(getArticlePersonalInfo()) { info, state ->
+            info ?: return@subscribeOnDataSource  null
+
+            state.copy(
+                isBookmark =  info.isBookmark,
+                isLike =  info.isLike
+            )
+        }
+
+        subscribeOnDataSource(repository.getAppSettings()) { settings, state ->
+            state.copy(
+                isDarkMode =  settings.isDarkMode,
+                isBigText = settings.isBigText
+            )
+        }
+    }
+
+    override fun getArticleContent(): LiveData<List<Any>?> {
+        return repository.loadArticleContent(articleId)
+    }
+
+    override fun getArticleData(): LiveData<ArticleData?> {
+        return repository.getArticle(articleId)
+    }
+
+    override fun getArticlePersonalInfo(): LiveData<ArticlePersonalInfo?> {
+        return repository.loadArticlePersonalInfo(articleId)
+    }
+
+    override fun handleNightMode() {
+        val settings = currentState.toAppSettings()
+        repository.updateSettings(settings.copy(
+            isDarkMode =  !settings.isDarkMode
+        ))
+    }
+
+    override fun handleUpText() {
+        val settings = currentState.toAppSettings()
+        repository.updateSettings(settings.copy(
+            isBigText = true
+        ))
+    }
+
+    override fun handleDownText() {
+        val settings = currentState.toAppSettings()
+        repository.updateSettings(settings.copy(
+            isBigText = false
+        ))
+    }
+
+    override fun handleBookmark() {
+        val toggleBookmark = {
+            val info = currentState.toArticlePersonalInfo()
+            repository.updateArticlePersonalInfo(info.copy(isBookmark = !info.isBookmark))
+        }
+
+        toggleBookmark()
+        val msg = if (currentState.isBookmark) "Add to bookmarks"
+        else { "Remove from bookmarks" }
+
+        notify(TextMessage(msg))
+    }
+
+    override fun handleLike() {
+        val toggleLike = {
+            val info = currentState.toArticlePersonalInfo()
+            repository.updateArticlePersonalInfo(info.copy(isLike = !info.isLike))
+        }
+
+        toggleLike()
+        val msg = if (currentState.isLike) Notify.TextMessage("Mark is liked")
+        else {
+            Notify.ActionMessage(
+                "Don`t like it anymore",
+                "No, still like it",
+                toggleLike // handle
+            )
+        }
+
+        notify(msg)
+    }
+
+    override fun handleShare() {
+        val msg = "Share is not implemented"
+        notify(Notify.ErrorMessage(msg, "OK", null))
+    }
+
+    override fun handleToggleMenu() {
+        updateState { it.copy(isShowMenu = !it.isShowMenu) }
+    }
+
+    override fun handleSearchMode(isSearch: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun handleSearch(query: String?) {
+        TODO("Not yet implemented")
+    }
+}
+
+data class ArticleState(
+    val isAuth: Boolean = false,
+    val isLoadingContent: Boolean = true,
+    val isLoadingReviews: Boolean = true,
+    val isLike: Boolean = false,
+    val isBookmark: Boolean = false,
+    val isShowMenu: Boolean = false,
+    val isBigText: Boolean = false,
+    val isDarkMode: Boolean = false,
+    val isSearch: Boolean = false,
+    val searchQuery: String? = null,
+    val searchResults: List<Pair<Int, Int>> = emptyList(),
+    val searchPosition: Int = 0,
+    val shareLink: String? = null,
+    val title: String? = null,
+    val category: String? = null,
+    val categoryIcon: Any? = null,
+    val date: String? = null,
+    val author: Any? = null,
+    val poster: String? = null,
+    val content: List<Any> = emptyList(),
+    val reviews: List<Any> = emptyList()
+)
